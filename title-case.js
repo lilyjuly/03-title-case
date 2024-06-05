88f174e4a08b0b7d600335af41570d8e23f51b8f
lily <lilyjulymm2009@gmail.com>
2024-06-05 13:50:13 +0630

    03-title-case

---
+++ title-case.js
@@ -0,0 +1,16 @@
+var titleCase = function(str) {
+    var result = [];
+  
+    var words = str.split(' ');
+  
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i].split('');
+  
+      word[0] = word[0].toUpperCase();
+  
+      result.push(word.join(''));
+    }
+  
+    return result.join(' ');
+  };
+  
